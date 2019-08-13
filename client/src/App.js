dae4ebf4914b68d7a560ea889567bd5ccda9c839
TansuD <51340289+TansuD@users.noreply.github.com>
2019-08-13 13:07:14 -0700

    new login folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,7 @@
 import React, { Component } from "react";
-import { Button, Form, Jumbotron, Container } from "react-bootstrap";
+import API from "./utils/API";
 class App extends Component {
   render() {
-    return (
-
-      <div>
-        <header>
-          <Jumbotron fluid>
-            <Container>
-              <h1>Wizard Game Login</h1>
-              <p>
-                Cast spells to defeat monsters!
-    </p>
-            </Container>
-          </Jumbotron>
-        </header>
-
-
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button id="playBtn" variant="primary" type="submit">
-            Play
-          </Button>
-          <br></br>
-          <Button id="createUserBtn" variant="primary" type="submit">
-            Create an account
-          </Button>
-        </Form>
-      </div>
-
-    );
   }
 }
 
--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import waterfall from "./App.css"
 import { Button, Form, Jumbotron, Container } from "react-bootstrap";
 import waterfall from "./login.css"
 
