bae27f78e8e208e0ee70ed0fad815621a9f4a972
TansuD <51340289+TansuD@users.noreply.github.com>
2019-08-13 14:42:19 -0700

    EOD 2  changes

---
+++ client/src/components/main-stats/main_stats.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button, Form, Jumbotron, Container } from "react-bootstrap";
+
+class MainStats extends Component {
+
+render(){
+    return (
+        <div>
+            jello
+        </div>
+    );
+  }
+}
+export default MainStats;
--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,6 @@ import registerServiceWorker from "./registerServiceWorker";
 
 
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
