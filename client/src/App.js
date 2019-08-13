ff1875219e66d6dc6ea7535a18708945c606f6c0
TansuD <51340289+TansuD@users.noreply.github.com>
2019-08-13 13:24:08 -0700

    react app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 import API from "./utils/API";
 class App extends Component {
-  render() {
+  state= {
+    monsters :[]
   }
+  
+  loadMonsters = () => {
+    API.getMonsters()
+      .then(res =>{
+          console.log(res.data)
+        this.setState({ monsters: res.data})
+      } )
+      .catch(err => console.log(err));
+  };
+  
+  componentDidMount(){
+    this.loadMonsters()
+  }
+  
+    render() {
+    }
 }
 
 export default App;
