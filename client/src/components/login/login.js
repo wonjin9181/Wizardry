86cde1985891cb38d64262a65df60582b61dd437
WonJin <wonjin9181@gmail.com>
2019-08-16 11:38:00 -0700

    change back

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -3,11 +3,64 @@ import React, { Component } from "react";
 import { Button, Form, Container, Row, Col } from "react-bootstrap";
 import "./login.css"
 import { Link, Redirect } from "react-router-dom";
+import API from "../../utils/API"
+
+class Login extends Component {
+
+    state = {
+        email: "",
+        password: "",
+        id:"",
+        play:false
+    }
 
 
-function Login(props) {
 
-    if (props.play === true) {
+    validateForm = () => {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+      }
+    
+      handleChange = event => {
+        event.preventDefault()
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+    
+      handleSubmit = event => {
+        let self = this;
+        event.preventDefault();
+        console.log(this.state)
+    
+        API.loginUser(this.state)
+          .then(function (result) {
+    
+            console.log(result)
+            let data = result.data
+            if (result.data.isUser) {
+              self.setState({
+                play: true,
+                password:"",
+                id: data.id
+              })
+              localStorage.setItem("key", self.state.id)
+            }
+            else {
+              alert('Incorrect Username and/or Password!');
+            }
+    
+          }).catch(err => {
+            alert(err);
+          })
+    
+      }
+    
+
+
+
+    render() {
+
+    if (this.state.play === true) {
         return <Redirect to="/main" />
     }
 
@@ -20,7 +73,7 @@ function Login(props) {
                     Cast spells to defeat monsters!
                 </p>
                 <aside id="loginaside">
-                    <Form className="loginForm" onSubmit={props.handleSubmit}>
+                    <Form className="loginForm" onSubmit={this.handleSubmit}>
                         <Form.Group as={Row} controlId="formHorizontalEmail">
                             <Form.Label column sm={2}>
                                 Email
@@ -31,8 +84,8 @@ function Login(props) {
                                     type="email"
                                     placeholder="Email"
                                     name="email"
-                                    value={props.email}
-                                    onChange={props.handleChange}
+                                    value={this.state.email}
+                                    onChange={this.handleChange}
                                 />
                             </Col>
                         </Form.Group>
@@ -47,8 +100,8 @@ function Login(props) {
                                     type="password"
                                     placeholder="Password"
                                     name="password"
-                                    value={props.password}
-                                    onChange={props.handleChange}
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
                                 />
                             </Col>
                         </Form.Group>
@@ -57,7 +110,7 @@ function Login(props) {
                             className="btn"
                             variant="primary"
                             type="submit"
-                            disabled={!props.validateForm()}>
+                            disabled={!this.validateForm()}>
                             Play
                              </Button>
 
@@ -72,7 +125,7 @@ function Login(props) {
         </div>
     );
 }
-
+}
 
 
 export default Login;
