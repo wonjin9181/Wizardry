ad64e4654ae82c31d3c3b99f48d9fb04e2caa6ba
WonJin <wonjin9181@gmail.com>
2019-08-19 15:21:17 -0700

    house member

--- routes/house-api.js
+++ routes/house-api.js
@@ -1,13 +1,15 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app.get("/api/house", function (req, res) {
+    app.get("/api/house/:house", function (req, res) {
+        
         db.House.findAll({
             where: {
                 houseName: req.params.house
             }
         })
             .then(function (dbHouse) {
+                console.log(dbHouse)
                 res.json(dbHouse)
             })
             .catch(function (err) {
