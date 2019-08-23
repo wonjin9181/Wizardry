e4f5fedfd1bb34387c88138b4494a4901362d868
Haig Talarian <haigtalarian@gmail.com>
2019-08-23 14:23:27 -0700

    more user avatars added

--- client/src/components/fight/fightAvatars.js
+++ client/src/components/fight/fightAvatars.js
@@ -2,7 +2,12 @@ const fightAvatars = [
     { src: 'images/rincewind-fight.png' },
     { src: 'images/blackwizard-fight.png'},
     { src: 'images/killer-fight.png'},
-    { src: 'images/spritey-fight.png'}
+    { src: 'images/spritey-fight.png'},
+    { src: 'images/gamora-fight.png'},
+    { src: 'images/drow-fight.png'},
+    { src: 'images/rydia-fight.png'},
+    { src: 'images/bluemage-fight.png'}
+    
 ];
 
 module.exports = fightAvatars;
